d836004d1b1eba70fe01d0cae0333a18d66de042
beckh <beckh@McKid>
2020-05-31 11:27:33 -0700

    String Methods(IP)

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,20 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length()>s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		
+		if(s.contains("underscores")) {
+				s =	s.replaceAll(" ", "_");
+		}
+		return s;
 	}
 
 	
@@ -46,23 +53,80 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		String[] temp1 = new String[2];
+		temp1 = s1.split(" ");
+		temp1[1].trim();
+		String[] temp2 = new String[2];
+		temp2 = s2.split(" ");
+		temp2[1].trim();
+		String[] temp3 = new String[2];
+		temp3 = s3.split(" ");
+		temp3[1].trim();
+		if(temp1[1].compareTo(temp2[1])>0&&temp1[1].compareTo(temp3[1])>0) {
+			return s1;
+		}
+		else if(temp2[1].compareTo(temp1[1])>0&&temp2[1].compareTo(temp3[1])>0) {
+			return s2;
+		}
+		else {
+		return s3;
+		}
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int end = 0;
+		for(int i = 0; i<s.length();i++) {
+			if(s.substring(i, i+1).equals("0")) {
+				end=end;
+			}
+			else if(s.substring(i, i+1).equals("1")) {
+				end+=1;
+			}
+			else if(s.substring(i, i+1).equals("2")) {
+				end+=2;
+			}
+			else if(s.substring(i, i+1).equals("3")) {
+				end+=3;
+			}
+			else if(s.substring(i, i+1).equals("4")) {
+				end+=4;
+			}
+			else if(s.substring(i, i+1).equals("5")) {
+				end+=5;
+			}
+			else if(s.substring(i, i+1).equals("6")) {
+				end+=6;
+			}
+			else if(s.substring(i, i+1).equals("7")) {
+				end+=7;
+			}
+			else if(s.substring(i, i+1).equals("8")) {
+				end+=8;
+			}
+			else if(s.substring(i, i+1).equals("9")) {
+				end+=9;
+			}
+		}
+		return end;
 	}
 	
 	
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int end = 0;
+		for(int i = 0; i<s.length();i++) {
+			if(s.substring(i, i+1).equals(substring)) {
+				end++;
+			}
+		}
+		return end;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
+	
 		return null;
 	}
 
@@ -75,7 +139,16 @@ public class StringMethods {
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int end = 0;
+		String[] temp;
+		temp = s.split(" ");
+		for(int i = 0; i<50;i++) {
+			if(temp[i].endsWith(substring)) {
+				end++;
+			}
+		}
+		System.out.println(end);
+		return end;
 	}
 	
 
